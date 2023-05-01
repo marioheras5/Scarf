caf2095f3898101abe6da8b15f450399b0b06ae3
Mario <marioheras02@gmail.com>
2023-05-01 15:57:19 +0200

    animaciones, movimiento, dash

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,26 +1,55 @@
+
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float speed = 5f;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    // Movimiento
+    public float moveSpeed = 5f;
+    Vector2 movement;
+
+    // Dash
+    public float dashCooldown = 0.35f;
+    public float dashTime = 0.15f;
+    public float dashForce = 25f;
+    bool canDash = true;
+    bool isDashing = false;
+    public Animator animator;
+    public Rigidbody2D rb;
 
     // Update is called once per frame
     void Update()
     {
-        MovePlayer();
-    }
-    void MovePlayer()
-    {
-        float horizontalInput = Input.GetAxis("Horizontal");
-        float verticalInput = Input.GetAxis("Vertical");
+        if (rb == null || animator == null || isDashing) return;
+        // Movimiento
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+        movement.Normalize();
+        animator.SetFloat("Speed", movement.sqrMagnitude);
 
-        transform.position += new Vector3(horizontalInput, verticalInput, 0) * speed * Time.deltaTime;
+        // Dash
+        if (Input.GetKeyDown(KeyCode.Space) && canDash && !(movement.x == 0 && movement.y == 0))
+        {
+            isDashing = true;
+            StartCoroutine(Dash());
+        }
+    }
+    IEnumerator Dash()
+    {
+        canDash = false;
+        rb.velocity = movement * dashForce;
+        yield return new WaitForSeconds(dashTime);
+        isDashing = false;
+        yield return new WaitForSeconds(dashCooldown);
+        canDash = true;
+    }
+    private void FixedUpdate()
+    {
+        if (rb == null || animator == null || isDashing) return;
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+        
     }
 }
